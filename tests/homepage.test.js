c594c9bbf843f2e189814bb103b4dcb9f9a97660
Lazontez <tezgardner1212@gmail.com>
2025-02-20 18:01:19 -0600

    Load time test

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -3,6 +3,7 @@ const edge = require('selenium-webdriver/edge');
 const { expect } = require('chai');
 
 describe('ESPN Homepage Test', function () {
+    this.timeout(30000); // Increase timeout
     let driver;
 
     before(async function () {
@@ -16,10 +17,16 @@ describe('ESPN Homepage Test', function () {
         }
     });
 
-    it('should load ESPN homepage', async function () {
+    it('Ensures homepage loads within 4 seconds ', async function () {
+        const start = performance.now();
         await driver.get('https://www.espn.com');
+        await driver.wait(until.elementLocated(By.css('img')), 15000);
+        const end = performance.now();
+        const time = end - start; 
+        expect(time).to.be.lessThan(4000, `Page load time was ${time} ms`);
         const title = await driver.getTitle();
         expect(title).to.include('ESPN');
     });
 });
+
 
